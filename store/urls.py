e91ca6fd4579f7eb56f67d0b59e150cb43ce0d80
AnandaThalia <Ananda_Thalia54@apps.ipb.ac.id>
2020-05-18 06:29:55 +0700

    menambahkan store

--- store/urls.py
+++ store/urls.py
@@ -1,15 +1,10 @@
 from django.conf.urls import url
-
 from . import views
 
 urlpatterns = [
-        #Leave as empty string for base url
-
-	# path('', views.store, name="store"),
-	url(r'^$', views.store, name="store"),
-	# path('cart/', views.cart, name="cart"),
-	url(r'^cart/$', views.cart, name="cart"),
-	# path('checkout/', views.checkout, name="checkout"),
-	url(r'^store/$', views.checkout, name="checkout"),
-
+    url(r'^$',views.store,name='store'),
+    url(r'^carts/$',views.cart,name='cart'),
+    url(r'^cekot/$',views.checkout,name='checkout'),
+    # url(r'^process_order/$',views.processOrder,name='process_order'),
+    
 ]
--- websiterpl/urls.py
+++ websiterpl/urls.py
@@ -1,14 +1,23 @@
 from django.conf.urls import url,include
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 
 from . import views
+from store import views as storeViews
 
 urlpatterns = [
     url(r'^artikel/', include('artikel.urls', namespace='artikel')),
     url(r'^store/', include('store.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.index, name='index'),
-
+    url(r'^cart/',include('store.urls')),
+    url(r'^checkout/',include('store.urls')),
+    url(r'^update_item/$',storeViews.updateItem,name='update_item'),
+    url(r'^process_order/$',storeViews.processOrder,name='process_order'),
     
+   
     
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
